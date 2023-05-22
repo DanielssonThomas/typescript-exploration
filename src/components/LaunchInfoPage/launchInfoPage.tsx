387f05b965213eff15a36b32e08be6e1e1dee249
DouglasLindahl <douglas.lindahl@gmail.com>
2023-05-22 12:49:43 +0200

    Update launchInfoPage.tsx

--- src/components/LaunchInfoPage/launchInfoPage.tsx
+++ src/components/LaunchInfoPage/launchInfoPage.tsx
@@ -29,7 +29,7 @@ function GetLaunch() {
     queryKey: ["repoData"],
     queryFn: () =>
       fetch(
-        `https://ll.thespacedevs.com/2.2.0/launch/upcoming/${id}/?format=json`
+        `https://lldev.thespacedevs.com/2.2.0/launch/upcoming/${id}/?format=json`
       ).then((res) => res.json()),
   });
 
