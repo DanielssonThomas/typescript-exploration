095282d77ef00b27a42e598fb1e6a83dc169a697
DouglasLindahl <douglas.lindahl@gmail.com>
2023-05-22 12:49:39 +0200

    a

--- src/components/LaunchInfoPage/launchInfoPage.tsx
+++ src/components/LaunchInfoPage/launchInfoPage.tsx
@@ -29,7 +29,7 @@ function GetLaunch() {
     queryKey: ["repoData"],
     queryFn: () =>
       fetch(
-        `https://lldev.thespacedevs.com/2.2.0/launch/upcoming/${id}/?format=json`
+        `https://ll.thespacedevs.com/2.2.0/launch/upcoming/${id}/?format=json`
       ).then((res) => res.json()),
   });
 
