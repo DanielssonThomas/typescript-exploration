2ddeb88335eb1bbd8548266f4933fb6ec388238d
DouglasLindahl <douglas.lindahl@gmail.com>
2023-05-22 12:45:37 +0200

    Update launchInfoPage.tsx

--- src/components/LaunchInfoPage/launchInfoPage.tsx
+++ src/components/LaunchInfoPage/launchInfoPage.tsx
@@ -1,9 +1,18 @@
 import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
+    QueryClient,
+    QueryClientProvider,
+    useQuery,
+  } from "@tanstack/react-query";
+  import { useParams } from 'react-router-dom';
+  import { styled } from "styled-components";
+
+
+  const Image = styled.img`
+  width: 20vw;
+  height: 15vh;
+  object-fit: cover;
+  object-position: center;
+`;
 
 export default function LaunchInfoPage() {
   const queryClient = new QueryClient();
@@ -28,11 +37,13 @@ function GetLaunch() {
 
   if (error) return <div>An error has occurred</div>;
 
-  const res: FetchSingleResult = data;
-  console.log(res);
-  return (
-    <div>
-      <h1>{res.name}</h1>
-    </div>
-  );
+
+      const res: FetchSingleResult = data;
+      console.log(res);
+      return (
+        <div>
+            <h1>{res.name}</h1>
+            <Image src={res.image} alt={res.name} />
+        </div>
+      );
 }
